7103ab2f629f6004b5666b95c9818c7ab2f31c61
000VIJAY <vijay893689@gmail.com>
2022-06-15 12:25:12 +0530

    added line equal greater or less comparison

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Welcome to line Comparison Program");
+﻿        Console.WriteLine("Welcome to line Comparison Program");
 
     int x1 = Convert.ToInt32(Console.ReadLine()), x2 = Convert.ToInt32(Console.ReadLine()),
         y1 = Convert.ToInt32(Console.ReadLine()), y2 = Convert.ToInt32(Console.ReadLine());
@@ -9,10 +9,10 @@
     double num2 = Math.Pow(z2, 2);
 double distance = Math.Sqrt(num1 + num2);
 
-Console.WriteLine("distance : " +distance);
+        Console.WriteLine("distance : " +distance);
 
 int a1 = Convert.ToInt32(Console.ReadLine()), a2 = Convert.ToInt32(Console.ReadLine()),
-        b1 = Convert.ToInt32(Console.ReadLine()), b2 = Convert.ToInt32(Console.ReadLine());
+    b1 = Convert.ToInt32(Console.ReadLine()), b2 = Convert.ToInt32(Console.ReadLine());
 
 int c1 = a2 - a1;
 int c2 = b2 - b1;
@@ -20,13 +20,17 @@ double numb1 = Math.Pow(c1, 2);
 double numb2 = Math.Pow(c2, 2);
 double distance1 = Math.Sqrt(numb1 + numb2);
 
-    Console.WriteLine("distance1 : " + distance1);
+        Console.WriteLine("distance1 : " + distance1);
 
-if (distance == distance1)
+if ( distance== distance1)
 {
-         Console.WriteLine("equal");
+        Console.WriteLine("equal");
+}
+else if (distance < distance1)
+{
+        Console.WriteLine("distance < distance1");
 }
 else
 {
-         Console.WriteLine("not equal");
+        Console.WriteLine("distance < distance1");
 }
