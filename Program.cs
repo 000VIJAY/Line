5d1463b59d5812f026ff88da4a9c2f5e6bbf88d0
000VIJAY <vijay893689@gmail.com>
2022-06-15 11:15:14 +0530

    added distance by cartesion method

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
 ﻿Console.WriteLine("Welcome to line Comparison Program");
+int x1 = Convert.ToInt32(Console.ReadLine()), x2 = Convert.ToInt32(Console.ReadLine()),
+    y1 = Convert.ToInt32(Console.ReadLine()), y2 = Convert.ToInt32(Console.ReadLine());
+int z1 = x2 - x1;
+int z2 = y2 - y1;
+double num1 = Math.Pow(z1, 2);
+double num2 = Math.Pow(z2, 2);
+double distance = Math.Sqrt(num1 + num2);
+Console.WriteLine("distance : " +distance);
